884000c69bfbab7eb28120d88b0669ade2bf63b3
jusukarneh <jusudaddi@yahoo.com>
2023-08-30 22:27:09 -0400

    save work

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+// Import our modular routers for /tips and /feedback
+const tipsRouter = require('./tips');
+const feedbackRouter = require('./feedback');
+const diagnosticRouter =require('./diagnostics');
+
+const app = express();
+
+app.use('/tips', tipsRouter);
+app.use('/tips', feedbackRouter);
+app.use('/tips', diagnosticRouter);
---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path')
+
+const api = require('./routes/index.js');
+
+const PORT = process.env.PORT 
+
+const app = express();
+
